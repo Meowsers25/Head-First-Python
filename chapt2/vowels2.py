a6345d2d6940d97422cf9af89f155add000268c9
Chris <c.lamothe25@gmail.com>
2018-08-07 07:13:06 -0400

    More working with lists

--- chapt2/vowels2.py
+++ chapt2/vowels2.py
@@ -7,3 +7,22 @@ for letter in word:  # iterates through each letter in word
             found.append(letter)  # add letter to found list
 for vowel in found:  # iterate through found list
     print(vowel)  # and print
+
+nums = [1, 2, 3, 4]
+print(nums)
+nums.remove(4)  # .remove() removes a specific value
+print(nums)
+
+
+nums2 = [1, 2, 3, 4]
+# print(nums2.pop())  # can pop() an index value
+removed = nums2.pop() + 4  # .pop() can be stored in a variable and reused
+print(removed)
+
+
+nums2.extend([5, 6, 7])  # .extend([]) adds a list wheras .append()
+print(nums2)  # takes single value
+
+nums2.insert(0, -1)  # .insert() inserts value at specified location (0 here)
+nums2.insert(4, 'four')
+print(nums2)
